d2036ee46208db8b86aba6cb479ed9c85ab5bfcf
McDonglious <sanderdemiddelaer@hotmail.com>
2026-02-26 22:22:06 +0100

    Added dynamic particle background with blue and gray color scheme.

--- Portfolio/src/app/app.ts
+++ Portfolio/src/app/app.ts
@@ -1,11 +1,20 @@
-import { Component, signal } from '@angular/core';
-import {Home} from './home/home';
-import {RouterLink, RouterOutlet} from '@angular/router';
+import { Component, signal, Inject, PLATFORM_ID } from '@angular/core';
+import {isPlatformBrowser, NgIf} from '@angular/common';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { MoveDirection, OutMode, Container, Engine } from "@tsparticles/engine";
+import { loadSlim } from "@tsparticles/slim";
+import { NgParticlesService, NgxParticlesModule } from "@tsparticles/angular";
 
 @Component({
   selector: 'app-root',
-  imports: [Home, RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, NgxParticlesModule, NgIf],
   template: `
+    <ngx-particles
+      *ngIf="isBrowser"
+      [id]="id"
+      [options]="particlesOptions"
+      (particlesLoaded)="particlesLoaded($event)"
+    ></ngx-particles>
     <main>
       <a [routerLink]="['/']">
         <header class="brand-name">
@@ -21,4 +30,100 @@ import {RouterLink, RouterOutlet} from '@angular/router';
 })
 export class App {
   protected readonly title = signal('Portfolio');
+  id = "tsparticles";
+  isBrowser = false;
+
+  particlesOptions = {
+    background: {
+      color: {
+        value: "#222222", // Dark grey background
+      },
+    },
+    fpsLimit: 120,
+    interactivity: {
+      events: {
+        onClick: {
+          enable: true,
+          mode: "push",
+        },
+        onHover: {
+          enable: true,
+          mode: "repulse",
+        },
+        resize: {
+          enable: true,
+        },
+      },
+      modes: {
+        push: {
+          quantity: 4,
+        },
+        repulse: {
+          distance: 200,
+          duration: 0.4,
+        },
+      },
+    },
+    particles: {
+      color: {
+        value: "#4dabf7", // Soft blue particles
+      },
+      links: {
+        color: "#74b9ff", // Lighter blue links
+        distance: 150,
+        enable: true,
+        opacity: 0.5,
+        width: 1,
+      },
+      move: {
+        direction: MoveDirection.none,
+        enable: true,
+        outModes: {
+          default: OutMode.bounce,
+        },
+        random: false,
+        speed: 6,
+        straight: false,
+      },
+      number: {
+        density: {
+          enable: true,
+          area: 800,
+        },
+        value: 80,
+      },
+      opacity: {
+        value: 0.7, // Slightly more opaque for better visibility
+      },
+      shape: {
+        type: "circle",
+      },
+      size: {
+        value: { min: 1, max: 5 },
+      },
+    },
+    detectRetina: true,
+  };
+
+
+  constructor(
+    private readonly ngParticlesService: NgParticlesService,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
+  }
+
+  ngOnInit(): void {
+    if (this.isBrowser) {
+      this.ngParticlesService.init(async (engine: Engine) => {
+        await loadSlim(engine);
+      });
+    }
+  }
+
+  particlesLoaded(container: Container): void {
+    console.log(container);
+  }
 }
+
+
