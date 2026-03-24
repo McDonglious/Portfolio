b380abe7fdd755f9f438b252c41a95ccad21f7c7
McDonglious <sanderdemiddelaer@hotmail.com>
2026-03-24 22:57:01 +0100

    using angular build in build script and removed update script as we're doing it in deploy.yml

--- update_and_deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /var/www/Portfolio/Portfolio
-git pull origin main  # Pull the latest code
-npm install           # Install dependencies
-npm run build:ssr     # Rebuild the app
-docker-compose down   # Stop current containers
-docker-compose up -d  # Restart with new code
