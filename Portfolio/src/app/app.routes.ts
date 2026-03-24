e5ffb661085161ce784c533d086684505cef8367
McDonglious <sanderdemiddelaer@hotmail.com>
2026-03-24 19:01:42 +0100

    fixes for prerendering dynamic routes.

--- Portfolio/src/app/app.routes.ts
+++ Portfolio/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import {Home} from './home/home';
 import {ProjectDetails} from './project-details/project-details';
+import { Type } from '@angular/core';
 
 export const routes: Routes = [{
     path: '',
@@ -11,5 +12,15 @@ export const routes: Routes = [{
     path: 'projects/:id',
     component: ProjectDetails,
     title: 'Projects Details',
-  }
+  },
+  {
+    path: 'projects/:id',
+    loadComponent: () =>
+      import('./project-details/project-details').then(
+        (m) => m.ProjectDetails as Type<unknown>
+      ),
+    data: {
+      prerender: false, // or true with staticParams
+    },
+  },
 ];
