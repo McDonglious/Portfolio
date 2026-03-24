8d24a6021f7a0a106f3a9fbd2db0a62c1122de13
McDonglious <sanderdemiddelaer@hotmail.com>
2026-03-24 19:17:12 +0100

    Add GitHub Actions deploy workflow

---
+++ update_and_deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /var/www/portfolio/Portfolio
+git pull origin main  # Pull the latest code
+npm install           # Install dependencies
+npm run build:ssr     # Rebuild the app
+docker-compose down   # Stop current containers
+docker-compose up -d  # Restart with new code
